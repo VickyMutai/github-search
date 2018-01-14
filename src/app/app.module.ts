a45ee860e707683eb3982b075efbbb5ac819b4e1
VickyMutai <vicky.mutai96@gmail.com>
2018-01-14 16:24:49 +0300

    Edit the landing page and search engine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,18 @@ import {ProfileService} from './services/profile.service';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
-import { SearchFormComponent } from './search-form/search-form.component';
 import { LandingComponent } from './landing/landing.component';
 import { RouterModule, Routes } from '@angular/router';
 //defining routes
-const routes:Routes = [
-  {path:"landing", component: LandingComponent};
-  {path:"profile", component: ProfileComponent};
+const routes:Routes=[
+  {path:"landing",component: LandingComponent},
+  {path:"profile",component: ProfileComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    SearchFormComponent,
     LandingComponent
   ],
   imports: [
